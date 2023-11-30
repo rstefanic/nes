0053317209a5555eb10072d2c9512aa89392b548
Robert Stefanic <rstefanic72@gmail.com>
2023-11-29 22:48:24 -0500

    main: Add memory dump relative to the CPU's program counter
    I wanted to have a way to step through the CPU and see where the program
    counter's at along with the data around it. This commit adds a "window"
    into memory so that I can see the surrounding bytes relative to the PC.
    It's sort of reminiscent to Visual Studios memory debugging tools.

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,11 @@ const raylib = @cImport({
 
 pub fn main() !void {
     var bus = Bus{};
-    _ = Cpu.init(&bus);
+    var cpu = Cpu.init(&bus);
+
+    var buffer: [1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
 
     raylib.SetConfigFlags(raylib.FLAG_VSYNC_HINT);
     raylib.InitWindow(800, 600, "NES");
@@ -16,8 +20,65 @@ pub fn main() !void {
     while (!raylib.WindowShouldClose()) {
         raylib.BeginDrawing();
         defer raylib.EndDrawing();
+        raylib.ClearBackground(raylib.BLACK);
 
-        raylib.ClearBackground(raylib.RAYWHITE);
-        raylib.DrawText("Work in progress...", 300, 250, 20, raylib.LIGHTGRAY);
+        // Events
+        if (raylib.IsKeyPressed(raylib.KEY_S) or raylib.IsKeyPressed(raylib.KEY_SPACE)) {
+            try cpu.step();
+        }
+
+        var start_row: u16 = undefined;
+        var end_row: u16 = undefined;
+        const pc = cpu.pc;
+
+        // We want to show five rows from memory at all times. Therefore, if
+        // the PC is within the first two rows or the last three rows, then we
+        // manually adjust the window to fix it at the start or end so that we
+        // don't overflow; otherwise we just calculate the start and end by
+        // taking two rows before the PC and three rows after the PC.
+        if (pc < 0x0020) {
+            start_row = 0x0000;
+            end_row = 0x0050;
+        } else if (pc > 0xFFC0) {
+            start_row = 0xFFA0;
+            end_row = 0xFFF0;
+        } else {
+            start_row = (pc & 0xFFF0) - 0x0020;
+            end_row = (pc & 0xFFF0) + 0x0030;
+        }
+
+        const mem = bus.memory[start_row..end_row];
+        const font_size = 38;
+        const x_spacing = 38;
+        const y_spacing = 38;
+        const x_padding = 10;
+        const address_start = 90;
+        var x_offset: c_int = x_padding;
+        var y_offset: c_int = 0;
+
+        for (mem, 0..) |m, i| {
+            const current_address = start_row + i;
+
+            if ((current_address & 0xFFF0) == current_address) {
+                x_offset = x_padding;
+                y_offset += y_spacing;
+                const fmt = try std.fmt.allocPrintZ(allocator, "{x:0>4}", .{current_address});
+                defer allocator.free(fmt);
+                const c_fmt: [*c]const u8 = @ptrCast(fmt);
+                raylib.DrawText(c_fmt, x_offset, y_offset, font_size, raylib.GRAY);
+                x_offset = address_start;
+            }
+
+            x_offset += x_spacing;
+            const fmt = try std.fmt.allocPrintZ(allocator, "{x}", .{m});
+            defer allocator.free(fmt);
+            const c_fmt: [*c]const u8 = @ptrCast(fmt);
+            const color = if (pc == current_address) raylib.WHITE else raylib.GRAY;
+            raylib.DrawText(c_fmt, x_offset, y_offset, font_size, color);
+        }
+
+        y_offset += y_spacing * 3;
+        const instruction_font_size = 24;
+        raylib.DrawText("Press \"s\" or \"<space>\" to step through the next instruction.", x_padding, y_offset, instruction_font_size, raylib.GRAY);
     }
 }
