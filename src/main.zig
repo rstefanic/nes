ee99f2442d523c92b4b107aa9f41c4db36736f1f
Robert Stefanic <rstefanic72@gmail.com>
2023-12-03 22:36:49 -0500

    main: Draw Cpu information to screen
    This will be useful for debugging. All of the CPU registers, cycles, and
    status flags are now displayed on the screen.
    
    There was a lot of memory allocation going on everytime we wanted to
    print something to the screen using `allocPrintZ` to format the string,
    so a helper function named `drawText` was created to proxy a call to
    raylib's `DrawText` function. It takes care of formating the string and
    freeing the memory. Options that are needed for raylib's `DrawText` are
    now handled through the `DrawTextOptions` struct.

--- src/main.zig
+++ src/main.zig
@@ -47,14 +47,19 @@ pub fn main() !void {
             end_row = (pc & 0xFFF0) + 0x0030;
         }
 
+        // Screen Drawing
+
         const mem = bus.memory[start_row..end_row];
-        const font_size = 32;
+        const mem_font_size = 32;
+        const instruction_font_size = 24;
         const x_spacing = 42;
         const y_spacing = 42;
         const x_padding = 10;
         const address_start = 80;
         var x_offset: c_int = x_padding;
         var y_offset: c_int = 0;
+
+        // Draw Memory
 
         for (mem, 0..) |m, i| {
             const current_address = start_row + i;
@@ -65,7 +70,7 @@ pub fn main() !void {
                 const fmt = try std.fmt.allocPrintZ(allocator, "{x:0>4}", .{current_address});
                 defer allocator.free(fmt);
                 const c_fmt: [*c]const u8 = @ptrCast(fmt);
-                raylib.DrawText(c_fmt, x_offset, y_offset, font_size, raylib.GRAY);
+                raylib.DrawText(c_fmt, x_offset, y_offset, mem_font_size, raylib.GRAY);
                 x_offset = address_start;
             }
 
@@ -74,11 +79,91 @@ pub fn main() !void {
             defer allocator.free(fmt);
             const c_fmt: [*c]const u8 = @ptrCast(fmt);
             const color = if (pc == current_address) raylib.WHITE else raylib.GRAY;
-            raylib.DrawText(c_fmt, x_offset, y_offset, font_size, color);
+            raylib.DrawText(c_fmt, x_offset, y_offset, mem_font_size, color);
         }
 
-        y_offset += y_spacing * 3;
-        const instruction_font_size = 24;
-        raylib.DrawText("Press \"s\" or \"<space>\" to step through the next instruction.", x_padding, y_offset, instruction_font_size, raylib.GRAY);
+        // Draw CPU Information
+
+        y_offset += y_spacing * 2;
+        var options: DrawTextOptions = .{ .pos_x = x_padding, .pos_y = y_offset, .font_size = instruction_font_size };
+        try drawText(allocator, "a: 0x{x:0>2}", .{cpu.a}, options);
+
+        options.pos_x += 300;
+        try drawText(allocator, "cycles: {d}", .{cpu.cycles}, options);
+
+        options.pos_x = x_padding;
+        options.pos_y += y_spacing;
+        try drawText(allocator, "x: 0x{x:0>2}", .{cpu.x}, options);
+
+        options.pos_x += 300;
+        try drawText(allocator, "pc: 0x{x:0>4}", .{cpu.pc}, options);
+
+        options.pos_x = x_padding;
+        options.pos_y += y_spacing;
+        try drawText(allocator, "y: 0x{x:0>2}", .{cpu.y}, options);
+
+        options.pos_x += 300;
+        try drawText(allocator, "sp: 0x{x:0>4}", .{cpu.sp}, options);
+
+        // Status Registers
+        options.pos_y += y_spacing;
+        options.pos_x = x_padding;
+        try drawText(allocator, "s:", .{}, options);
+
+        options.pos_x += x_spacing;
+        options.color = flagColor(cpu.status.negative_result);
+        try drawText(allocator, "N", .{}, options);
+
+        options.pos_x += x_spacing;
+        options.color = flagColor(cpu.status.overflow);
+        try drawText(allocator, "V", .{}, options);
+
+        options.pos_x += x_spacing;
+        options.color = raylib.GRAY;
+        try drawText(allocator, "-", .{}, options);
+
+        options.pos_x += x_spacing;
+        options.color = raylib.GRAY;
+        try drawText(allocator, "-", .{}, options);
+
+        options.pos_x += x_spacing;
+        options.color = flagColor(cpu.status.decimal_mode);
+        try drawText(allocator, "D", .{}, options);
+
+        options.pos_x += x_spacing;
+        options.color = flagColor(cpu.status.interrupt_disable);
+        try drawText(allocator, "I", .{}, options);
+
+        options.pos_x += x_spacing;
+        options.color = flagColor(cpu.status.zero_result);
+        try drawText(allocator, "Z", .{}, options);
+
+        options.pos_x += x_spacing;
+        options.color = flagColor(cpu.status.carry);
+        try drawText(allocator, "C", .{}, options);
+
+        // Draw instructions
+        options.pos_y += y_spacing * 2;
+        options.pos_x = x_padding;
+        options.color = raylib.GRAY;
+        try drawText(allocator, "Press \"s\" or \"<space>\" to step through the next instruction.", .{}, options);
     }
+}
+
+inline fn flagColor(state: bool) raylib.Color {
+    return if (state) raylib.GREEN else raylib.RED;
+}
+
+const DrawTextOptions = struct {
+    pos_x: c_int = 0,
+    pos_y: c_int = 0,
+    font_size: c_int = 12,
+    color: raylib.Color = raylib.GRAY,
+};
+
+fn drawText(allocator: std.mem.Allocator, comptime fmt: []const u8, args: anytype, options: DrawTextOptions) !void {
+    const buf = try std.fmt.allocPrintZ(allocator, fmt, args);
+    defer allocator.free(buf);
+    var c_buf: [*c]const u8 = @ptrCast(buf); // Cast it to a C style pointer so raylib can use it
+    raylib.DrawText(c_buf, options.pos_x, options.pos_y, options.font_size, options.color);
 }
