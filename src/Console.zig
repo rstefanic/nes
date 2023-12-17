44738fb08fb2fd5e880238c7f52f163ec0cb1d68
Robert Stefanic <rstefanic72@gmail.com>
2023-12-16 22:48:17 -0500

    Console: Add reading/writing to cartridge PRG ROM banks
    I'm not quite sure if this is right, but we'll roll with it for now. If
    the PRG ROM size is 1, then our PRG ROM is only 16 KB, but the
    addressable space is 32 KB. For Mapper 0 (which is what I'm trying to
    imlpement), the data from the first 16 KB are mirrored giving us the
    total range of 32 KB. So if the PRG ROM size is 1, and we're trying to
    read from an address that's larger than 16 KB, then we need to read it's
    mirror from the 16 KB that are available.
    
    I'm not sure if I should modify Cartridge so that we encode the only two
    possibilities for addressing the cartridge space. On the one hand, the
    iNES spec says nothing about limiting the addressable space. It just
    says that you multiple the PRG ROM size read from the header by 16 KB to
    get the size of the cartridge; however, in practice, there are only 32
    KB of addressable space... Hmmmm....

--- src/Console.zig
+++ src/Console.zig
@@ -9,7 +9,10 @@ cpu: ?*Cpu = null,
 cartridge: ?*Cartridge = null,
 memory: [0x800]u8 = std.mem.zeroes([0x800]u8),
 
-const ConsoleError = error{InvalidMemoryAddress};
+const ConsoleError = error{
+    InvalidMemoryAddress,
+    MissingCartridge,
+};
 
 pub fn step(self: *Console) !void {
     try self.cpu.?.step();
@@ -26,6 +29,16 @@ pub fn connectCartridge(self: *Console, cartridge: *Cartridge) void {
 pub fn read(self: *Console, address: u16) !u8 {
     if (address >= 0x0000 and address <= 0x1FFF) {
         return self.memory[address & 0x07FF];
+    } else if (address >= 0x8000 and address <= 0xFFFF) {
+        if (self.cartridge) |cartridge| {
+            if (cartridge.header.prg_rom_size == 1) {
+                return self.cartridge.?.prg_rom_bank[address & 0x3FFF];
+            }
+
+            return self.cartridge.?.prg_rom_bank[address & 0x7FFF];
+        } else {
+            return ConsoleError.MissingCartridge;
+        }
     }
 
     return ConsoleError.InvalidMemoryAddress;
@@ -35,6 +48,18 @@ pub fn write(self: *Console, address: u16, value: u8) !void {
     if (address >= 0x0000 and address <= 0x1FFF) {
         self.memory[address & 0x07FF] = value;
         return;
+    } else if (address >= 0x8000 and address <= 0xFFFF) {
+        if (self.cartridge) |cartridge| {
+            if (cartridge.header.prg_rom_size == 1) {
+                self.cartridge.?.prg_rom_bank[address & 0x3FFF] = value;
+                return;
+            }
+
+            self.cartridge.?.prg_rom_bank[address & 0x7FFF] = value;
+            return;
+        } else {
+            return ConsoleError.MissingCartridge;
+        }
     }
 
     return ConsoleError.InvalidMemoryAddress;
